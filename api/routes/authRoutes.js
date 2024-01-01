3873052246c0ccce5efde6ee738bb04e25ac5b21
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-01 13:01:24 +0530

    added sign in route

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { signup, signin } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/sign-up", signup);
+authRouter.post("/sign-in", signin);
 
 export default authRouter;
