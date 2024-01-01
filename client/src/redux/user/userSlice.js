9e9546f61736052474248e9785bdd20dc1dd2076
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-01 19:31:10 +0530

    set up user slice

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  isLoading: false,
+  error: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.isLoading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isLoading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload ?? true;
+    },
+  },
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export default userSlice.reducer;
