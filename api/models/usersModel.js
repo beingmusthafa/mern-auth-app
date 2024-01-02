80d4dccad09945eb275e79681118d531dcbc327f
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-02 13:46:00 +0530

    profile image field added

--- api/models/usersModel.js
+++ api/models/usersModel.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const usersSchema = mongoose.Schema(
   {
-    nickname: {
+    username: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       unique: true,
@@ -14,6 +15,11 @@ const usersSchema = mongoose.Schema(
     password: {
       type: String,
       required: true,
+    },
+    profileImage: {
+      type: String,
+      default:
+        "https://camo.githubusercontent.com/c6fe2c13c27fe87ac6581b9fe289d2f071bd1b4ef6f3e3c5fc2aba0bbc23fd88/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f372f37632f50726f66696c655f6176617461725f706c616365686f6c6465725f6c617267652e706e67",
     },
   },
   { timestamps: true }
