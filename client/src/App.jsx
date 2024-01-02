fa17666fc64c6c97dbc8af79668768aba0f8eb28
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-02 13:46:42 +0530

    making profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -18,7 +19,9 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-500 flex justify-between p-4 text-white">
       <div className="font-bold">Auth</div>
       <ul className="flex gap-4">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/sign-in">Sign in</Link>
+        {currentUser ? (
+          <Link to="/profile">
+            <img
+              className="h-7 w-7 rounded-full object-cover"
+              src={currentUser.profileImage}
+              alt=""
+            />
+          </Link>
+        ) : (
+          <Link to="/sign-in">Sign in</Link>
+        )}
       </ul>
     </div>
   );
