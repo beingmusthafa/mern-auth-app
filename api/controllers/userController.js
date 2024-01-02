e2cd78adb7e417a3727226fb3962877755ed030c
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-02 23:15:07 +0530

    update profile image

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,16 @@
-export const test = (req, res) => {
-  res.send("user");
+import Users from "../models/usersModel.js";
+import { customError } from "../utils/error.js";
+
+export const updateProfileImage = async (req, res, next) => {
+  const url = req.body.imageUrl;
+  console.log(url);
+  console.log(req.session.user);
+  try {
+    await Users.findByIdAndUpdate(req.session.user._id, { profileImage: url });
+    res
+      .status(200)
+      .json({ success: true, message: "Profile pic updated successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
