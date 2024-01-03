5338c9e23298f31fe22adee42ba088639e018bde
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-03 23:25:17 +0530

    added placeholder image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex w-screen h-screen flex-col items-center">
+      <img
+        src="https://miro.medium.com/v2/resize:fit:678/0*kxPYwfJmkXZ3iCWy.png"
+        alt=""
+        className="h-50 mx-auto"
+      />
+    </div>
+  );
 };
 
 export default Home;
