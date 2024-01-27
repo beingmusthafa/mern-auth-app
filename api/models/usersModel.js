4d153a107e874b57018047fff5a623f4a9d5cd45
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-01-27 16:11:55 +0530

    default image fix

--- api/models/usersModel.js
+++ api/models/usersModel.js
@@ -19,7 +19,7 @@ const usersSchema = mongoose.Schema(
     profileImage: {
       type: String,
       default:
-        "https://camo.githubusercontent.com/c6fe2c13c27fe87ac6581b9fe289d2f071bd1b4ef6f3e3c5fc2aba0bbc23fd88/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f372f37632f50726f66696c655f6176617461725f706c616365686f6c6465725f6c617267652e706e67",
+        "https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg",
     },
     externalAuth: {
       type: Boolean,
